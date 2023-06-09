6a8090e86510bb2c4bc21b9a44a0efa487a6c9dd
Andrew Knotts <knottsan2010@gmail.com>
2023-06-09 18:00:10 +0900

    added items

--- src/scripts.js
+++ src/scripts.js
@@ -2,7 +2,9 @@ import * as THREE from 'https://unpkg.com/three@0.127.0/build/three.module.js';
 
 import {OrbitControls} from 'https://unpkg.com/three@0.127.0/examples/jsm/controls/OrbitControls.js';
 
+import {GLTFLoader} from 'https://unpkg.com/three@0.127.0/examples/jsm/loaders/GLTFLoader.js';
 
+//import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader'
 
 var renderer;
 
@@ -86,9 +88,10 @@ cube = new THREE.Mesh(cubeGeo, cubeMat)
 cube2 = new THREE.Mesh(cubeCanvas, cubeMat)
 scene.add(cube)
 scene.add(cube2)
-cube.position.set(0,4,0)
+cube.position.set(0,30,0)
 cube2.position.set(0,10,-9.5)
 cube.castShadow = true
+cube2.castShadow = true
 
 const ambient = new THREE.AmbientLight(0x333333)
 scene.add(ambient)
@@ -109,7 +112,7 @@ renderer = new THREE.WebGLRenderer( { antialias: true } );
 
 
 
-const planeGeometry = new THREE.PlaneGeometry(20,20)
+const planeGeometry = new THREE.BoxGeometry(20,20, 1)
 const planeMaterial = new THREE.MeshStandardMaterial({color: 0xfffff0, side: THREE.DoubleSide})
 const plane = new THREE.Mesh(planeGeometry, planeMaterial)
 scene.add(plane)
@@ -137,7 +140,45 @@ directionalLight.position.set(50, 40, 30)
 directionalLight.castShadow = true;
 directionalLight.shadow.camera.bottom = -12;
 
+const spotlight = new THREE.SpotLight(0xFFFFf0)
+scene.add(spotlight)
+spotlight.position.set(0,150,150)
+spotlight.castShadow = true;
+spotlight.angle = .03
+spotlight.penumbra = 1;
+spotlight.intensity = .6
 
+
+const targetObject = new THREE.Object3D(); 
+scene.add(targetObject);
+
+spotlight.target = targetObject;
+targetObject.position.set(0,17,0)
+
+
+const gloader = new GLTFLoader();
+
+gloader.load( './assets/bench/scene.gltf', function ( gltf ) {
+
+   const model = gltf.scene;
+	scene.add( gltf.scene );
+   gltf.scene.scale.set(50,50,50)
+   gltf.scene.position.set(0,1,2)
+   gltf.scene.rotation.y = -.6 * Math.PI
+   
+   model.traverse(function(node) {
+      if(node.isMesh)
+         node.castShadow = true;
+   })
+
+}, undefined, function ( error ) {
+
+	console.error( error );
+
+} );
+
+//const slh = new THREE.SpotLightHelper(spotlight);
+//scene.add(slh)
 
 
 var orbit = new OrbitControls(camera, renderer.domElement)
